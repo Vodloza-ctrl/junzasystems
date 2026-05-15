9271ed0c69053d82c8c19955da70bbcaccca59a8
Vodloza-ctrl <junzatv@gmail.com>
2026-05-15 17:38:34 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-const WNUM='263719362231';
-function wa(text){return `https://wa.me/${WNUM}?text=${encodeURIComponent(text)}`}
-document.querySelectorAll('[data-wa]').forEach(a=>a.href=wa(a.dataset.wa));
-const hamb=document.querySelector('.hamb'), links=document.querySelector('.links');
-if(hamb&&links){hamb.addEventListener('click',()=>links.classList.toggle('open'))}
