a34586ba87e8274e0ba5db9425c8cc64fb182c13
Vodloza-ctrl <junzatv@gmail.com>
2026-05-16 03:24:47 +0200

    Add files via upload

---
+++ public/assets/js/config.js
@@ -0,0 +1,8 @@
+window.SALE_CONFIG = {
+  apiBase: "https://api.sale.co.zw",
+  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  whatsappNumber: "263XXXXXXXXX",
+  assetsBase: "https://assets.sale.co.zw",
+  demoTenant: "nats-hair-lab"
+};
