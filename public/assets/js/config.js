1095fac20f45857a1b3881ed236582050bd6b09f
Vodloza-ctrl <junzatv@gmail.com>
2026-05-16 04:37:06 +0200

    Update config.js

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,7 +1,7 @@
 window.SALE_CONFIG = {
-  apiBase: "https://api.sale.co.zw",
-  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  apiBase: "https://sale-company-api.yasibomedia.workers.dev",
+  supabaseUrl: "",
+  supabaseAnonKey: "",
   whatsappNumber: "263XXXXXXXXX",
   assetsBase: "https://assets.sale.co.zw",
   demoTenant: "nats-hair-lab"
